225b85d19fca484995c49441b2db46a8e6532c4d
Tumbichi <germanviglietti@gmail.com>
2022-08-22 23:28:26 -0300

    fix: add configuration for fontawesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import { Layout } from "../template";
+import Navbar from "../features/Navbar/Navbar";
+
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+import ShoppingCartProvider from "../features/ShoppingCart/context/ShoppingCartProvider";
+config.autoAddCss = false;
 
 const theme = extendTheme({
   colors: {
@@ -14,13 +20,18 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      {router.route.startsWith("/admin") ? (
-        <Component {...pageProps} />
-      ) : (
-        <Layout>
+      <ShoppingCartProvider>
+        {router.route.startsWith("/admin") ? (
           <Component {...pageProps} />
-        </Layout>
-      )}
+        ) : (
+          <Layout>
+            {{
+              header: <Navbar />,
+              content: <Component {...pageProps} />,
+            }}
+          </Layout>
+        )}
+      </ShoppingCartProvider>
     </ChakraProvider>
   );
 }
